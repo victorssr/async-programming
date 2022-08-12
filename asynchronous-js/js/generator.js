26e4143ac8fe055cac4112aed6c6390f77a096e0
Victor Savoi Silvestrini Rodrigues <victor.savoi@bs2tecnologia.com.br>
2022-08-12 11:25:52 -0300

    Melhoria no cenário

--- asynchronous-js/js/generator.js
+++ asynchronous-js/js/generator.js
@@ -1,9 +1,12 @@
-function buscarRegistroComSyscall() {
-  return { valor: 1 };
+function buscarRegistro() {
+  const registro = { valor: 1 };
+  console.log(`Registro encontrado ${JSON.stringify(registro)}`);
+  return registro;
 }
 
-function somarValorDoRegistro(registro, novoValor) {
-  registro.valor += novoValor;
+function somarValorDoRegistro(registro, somaValor) {
+  registro.valor += somaValor;
+  console.log(`Registro somado ${JSON.stringify(registro)}`);
   return registro;
 }
 
@@ -11,38 +14,25 @@ function notificarCliente(registroSomado) {
   return { mensagem: `O valor final foi ${registroSomado.valor}` };
 }
 
-function* executar(id) {
-  const novoValor = 2;
+function* executar() {
+  const somaValor = 1;
 
   try {
-    console.log(`Iniciar buscarRegistroComSyscall, id: ${id}`);
-    const registro = yield buscarRegistroComSyscall();
-    console.log(
-      `Registro encontrado ${JSON.stringify(registro.value)}, id: ${id}`
-    );
-
-    const registroSomado = yield somarValorDoRegistro(
-      registro.value,
-      novoValor
-    );
-    console.log(
-      `Registro somado ${JSON.stringify(registroSomado.value)}, id: ${id}`
-    );
-
+    console.log(`Iniciar buscarRegistro()`);
+    const registro = yield buscarRegistro();
+    console.log(`Iniciar somarValorDoRegistro()`);
+    const registroSomado = yield somarValorDoRegistro(registro.value, somaValor);
     const notificacao = yield notificarCliente(registroSomado.value);
-
-    console.log(`Cliente notificado: ${notificacao.value.mensagem}, id: ${id}`);
+    console.log(`Cliente notificado: ${notificacao.value.mensagem}`);
   } catch (erro) {
-    console.log(`Ocorreu um problema: ${erro}, id: ${id}`);
+    console.log(`Ocorreu um problema: ${erro}`);
   }
 }
 
 function chamada() {
-  const generator = executar("1");
+  const generator = executar();
   const registro = generator.next();
-
   console.log("Fim da chamada");
-
   const registroSomado = generator.next(registro);
   const notificacao = generator.next(registroSomado);
   generator.next(notificacao);
