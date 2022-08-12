80e244b4b91e690e36b4cab8321253719ee95ea9
Victor Savoi Silvestrini Rodrigues <victor.savoi@bs2tecnologia.com.br>
2022-08-12 13:23:41 -0300

    Adaptando Program para executar os cenários

--- asynchronous-dot-net/Program.cs
+++ asynchronous-dot-net/Program.cs
@@ -1,43 +1,9 @@
-﻿await Chamada();
+﻿using Cenarios;
 
-async Task Chamada()
-{
-    Console.WriteLine("Iniciando chamada...");
-    await Execucao().ContinueWith((test) =>
-    {
-        Console.WriteLine("Fim da task");
-    });
-    Console.WriteLine("Fim da chamada");
-}
+CallbackCenario cenario = new();
 
-async Task Execucao()
-{
-    Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-    var task = Task.Run(() =>
-    {
-        Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-        FuncaoDemorada(4);
-        Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-    });
-    Console.WriteLine("Fim da execução");
-    Console.WriteLine($"Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-    await task;
-}
-
-
-Task FuncaoDemorada(int seconds)
-{
-    Console.WriteLine("Início da função demorada");
-
-    var date = DateTime.Now;
-    DateTime currentDate;
-
-    do
-    {
-        currentDate = DateTime.Now;
-    } while (currentDate.Subtract(date).Seconds < seconds);
-
-    Console.WriteLine("Fim da função demorada");
-
-    return Task.CompletedTask;
-}
+Console.WriteLine($"Iniciando programa... Thread: {Thread.CurrentThread.ManagedThreadId}");
+cenario.Executar();
+// var execucao = cenario.ExecutarAsync();
+Console.WriteLine($"Fim do programa. Thread: {Thread.CurrentThread.ManagedThreadId}");
+// await execucao;
