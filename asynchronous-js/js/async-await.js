29b6385ecabff30defb41af44dfcc8862b82b46b
Victor Savoi Silvestrini Rodrigues <victor.savoi@bs2tecnologia.com.br>
2022-08-12 11:09:33 -0300

    Cenário aprimorado

--- asynchronous-js/js/async-await.js
+++ asynchronous-js/js/async-await.js
@@ -1,61 +1,32 @@
-const indexErro = 3;
-const excecao = true;
-
 function buscarRegistro() {
-  return new Promise((resolve, reject) => {
-    if (indexErro == 0) {
-      if (excecao) {
-        throw Error("Erro exceção");
-      }      
-
-      reject(`Erro buscarRegistro()`);
-    } else {      
-      resolve({ valor: 1 });
-    }
+  return new Promise((resolve) => {
+    const registro = { valor: 1 };
+    console.log(`Registro encontrado ${JSON.stringify(registro)}`);
+    resolve(registro);
   });
 }
 
-function somarValorDoRegistro(registro, novoValor) {
-  return new Promise((resolve, reject) => {
-    if (indexErro == 1) {
-      if (excecao) {
-        throw Error("Erro exceção");
-      }
-
-      reject(`Erro somarValorDoRegistro()`);
-    } else {
-      registro.valor += novoValor;
-      resolve(registro);
-    }
+function somarValorDoRegistro(registro, somaValor) {
+  return new Promise((resolve) => {
+    registro.valor += somaValor;
+    console.log(`Registro somado ${JSON.stringify(registro)}`);
+    resolve(registro);
   });
 }
 
 function notificarCliente(registroSomado) {
-  return new Promise((resolve, reject) => {
-    if (indexErro == 2) {
-      if (excecao) {
-        throw Error("Erro exceção");
-      }
-
-      reject(`Erro somarValorDoRegistro()`);
-    } else {
-      resolve({ mensagem: `O valor final foi ${registroSomado.valor}` });
-    }
+  return new Promise((resolve) => {
+    resolve({ mensagem: `O valor final foi ${registroSomado.valor}` });
   });
 }
 
 async function executar() {
-  const novoValor = 2;
+  const somaValor = 1;
 
   try {
     const registro = await buscarRegistro();
-    console.log(`Registro encontrado ${JSON.stringify(registro)}`);
-
-    const registroSomado = await somarValorDoRegistro(registro, novoValor);
-    console.log(`Registro somado ${JSON.stringify(registroSomado)}`);
-
+    const registroSomado = await somarValorDoRegistro(registro, somaValor);
     const notificacao = await notificarCliente(registroSomado);
-
     console.log(`Cliente notificado: ${notificacao.mensagem}`);
   } catch (erro) {
     console.log(`Ocorreu um problema: ${erro}`);
