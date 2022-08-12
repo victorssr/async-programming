7f64f92cc7580ebc3b6b33aeabff618a90a287b2
Victor Savoi Silvestrini Rodrigues <victor.savoi@bs2tecnologia.com.br>
2022-08-12 12:30:09 -0300

    Novo campo

--- asynchronous-dot-net/Modelos/Registro.cs
+++ asynchronous-dot-net/Modelos/Registro.cs
@@ -2,5 +2,11 @@ namespace Modelos;
 
 public class Registro
 {
+    public Registro()
+    {
+        IdCliente = 1;
+    }
+    
+    public int IdCliente { get; set; }
     public int Valor { get; set; }
 }
